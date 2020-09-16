379f15a696c7e3f9362611d59e7572915f916b5c
jaebee94 <woqls8449@gmail.com>
2020-09-17 00:18:42 +0900

    Complete food classification model

---
+++ CNN/model.py
@@ -0,0 +1,68 @@
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, Conv2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+import numpy as np
+import os
+
+# 카테고리 지정
+foods_dir = "../datasets/train"
+foodnames = os.listdir(foods_dir)
+classes_number = len(foodnames)
+
+# 이미지 크기 지정하기
+image_w = 64
+image_h = 64
+
+# 데이터 열기 
+X_train, X_test, y_train, y_test = np.load("../datasets/dataset.npy", allow_pickle=True)
+
+# 데이터 정규화하기(0~1사이로)
+X_train = X_train.astype("float") / 256
+X_test  = X_test.astype("float")  / 256
+
+# 모델 구조 정의 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 전결합층
+model.add(Flatten())    # 벡터형태로 reshape
+model.add(Dense(512))   # 출력
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(classes_number))
+model.add(Activation('softmax'))
+
+# 모델 구축하기
+model.compile(loss='categorical_crossentropy',   # 최적화 함수 지정
+    optimizer='rmsprop',
+    metrics=['accuracy'])
+
+# 모델 확인
+print(model.summary())
+
+# 학습 완료된 모델 저장
+hdf5_file = "./food_model.hdf5"
+if os.path.exists(hdf5_file):
+    # 기존에 학습된 모델 불러들이기
+    model.load_weights(hdf5_file)
+else:
+    # 학습한 모델이 없으면 파일로 저장
+    model.fit(X_train, y_train, batch_size=32, epoch=10)
+    model.save_weights(hdf5_file)
+
+
+# 모델 평가하기 
+score = model.evaluate(X_test, y_test)
+print('loss=', score[0])        # loss
+print('accuracy=', score[1])    # acc
--- CNN/test.py
+++ CNN/test.py
@@ -1,80 +1,40 @@
-from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from PIL import Image
+import os, glob
+import numpy as np
 from sklearn.model_selection import train_test_split
-import pandas as pd
-import pickle
 
-foods = pd.read_excel('..\\s03p22a411\\datasets\\nutrition.xlsx', usecols=['식품명'])
-foods["image_paths"] = '..\\s03p22a411\\datasets\\images' + '\\' + foods["식품명"]
+import os
 
-X, y = foods["image_paths"], foods["식품명"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+foods_dir = "./datasets/train"
+foodnames = os.listdir(foods_dir)
+classes_number = len(foodnames)
 
-train_dataset = pd.merge(X_train, y_train, left_index=True, right_index=True)
-val_dataset = pd.merge(X_val, y_val, left_index=True, right_index=True)
+image_w = 64
+image_h = 64
+pixels = image_w * image_h * 3
 
-with open('.\\datasets\\train_dataset.pickle', 'wb') as file:
-    pickle.dump(train_dataset, file)
-with open('.\\datasets\\val_dataset.pickle', 'wb') as file:
-    pickle.dump(val_dataset, file)
+X = []
+Y = []
+for idx, foodname in enumerate(foodnames):
+    label = [0 for _ in range(classes_number)]
+    label[idx] = 1
+
+    image_dir = foods_dir + "/" + foodname
+    files = glob.glob(image_dir + "/*.jpg")
+    for i, f in enumerate(files):
+        img = Image.open(f)
+        img = img.convert("RGB")
+        img = img.resize((image_w, image_h))
+        data = np.asarray(img)
+        X.append(data)
+        Y.append(label)
+    print('{} / {}, {} preprocess complete.'.format(idx, classes_number, foodname))
+X = np.array(X)
+Y = np.array(Y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
+xy = (X_train, X_test, y_train, y_test)
 
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(2))
-model.add(Activation('softmax'))
-
-batch_size = 16
-
-# train_datagen = ImageDataGenerator(
-#         rescale=1./255,
-#         shear_range=0.2,
-#         zoom_range=0.2,
-#         horizontal_flip=True)
-# validation_datagen = ImageDataGenerator(rescale=1./255)
-# test_datagen = ImageDataGenerator(rescale=1./255)
-
-# train_generator = train_datagen.flow_from_directory(
-#         'data/train',
-#         target_size=(150, 150),
-#         batch_size=batch_size,
-#         class_mode='binary')
-
-# validation_generator = validation_datagen.flow_from_directory(
-#         'data/validation',
-#         target_size=(150, 150),
-#         batch_size=batch_size,
-#         class_mode='binary')
-
-# test_generator = test_datagen.flow_from_directory(
-#         'data/validation',
-#         target_size=(150, 150),
-#         batch_size=batch_size,
-#         class_mode='binary')
-
-# model.compile(
-#         loss='categorical_crossentropy',
-#         optimizer='rmsprop',
-#         metrics=['accuracy'])
-
-# model.fit_generator(
-#         train_generator,
-#         steps_per_epoch=1000//batch_size,
-#         validation_data=validation_generator,
-#         epochs=50)
-
-# model.save_weights('test_model.h5')
+np.save("./datasets/dataset.npy", xy)
+print('save complete!')
