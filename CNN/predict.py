98974fd6cbfe769862d8015b44cff5fba4ada3be
jaebee94 <woqls8449@gmail.com>
2020-09-17 19:46:12 +0900

    Fix bug

--- CNN/predict.py
+++ CNN/predict.py
@@ -38,15 +38,15 @@ X_test  = X_test.astype("float")  / 255
 print(X_train.shape[1:])
 # 모델 구조 정의 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Conv2D(64, (3, 3), input_shape=X_train.shape[1:], padding='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), padding='same'))
 model.add(Activation('relu'))
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(256, (3, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
