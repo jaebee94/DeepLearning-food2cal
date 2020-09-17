3cda44f5fea22468a24a1d563de1f6d1554e2224
jaebee94 <woqls8449@gmail.com>
2020-09-17 23:28:48 +0900

    Fix dense

--- CNN/model.py
+++ CNN/model.py
@@ -67,8 +67,8 @@ model.add(Activation('relu'))
 # model.add(Activation('relu'))
 model.add(Dropout(0.5))
 
-# model.add(Dense(classes_number))
-model.add(Dense(2))
+model.add(Dense(classes_number+1))
+# model.add(Dense(2))
 model.add(Activation('softmax'))
 
 # 모델 구축하기
