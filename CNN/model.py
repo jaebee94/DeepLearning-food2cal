69dfbe593dfa58a2ea487c1b90b0598ad47f89b1
jaebee94 <woqls8449@gmail.com>
2020-09-17 17:06:35 +0900

    Add version 1.0.1

--- CNN/model.py
+++ CNN/model.py
@@ -1,3 +1,4 @@
+from keras import optimizers
 from keras.models import Sequential
 from keras.layers import MaxPooling2D, Conv2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -27,30 +28,32 @@ food_list = os.listdir(foods_dir)
 ############################
 
 classes_number = len(food_list)
-
+print('category : ', classes_number, len(classes_number))
 # 데이터 열기 
 X_train, X_test, y_train, y_test = np.load("../data/dataset.npy", allow_pickle=True)
 
 # 데이터 정규화하기(0~1사이로)
-X_train = X_train.astype("float") / 256
-X_test  = X_test.astype("float")  / 256
+X_train = X_train.astype("float") / 255
+X_test  = X_test.astype("float")  / 255
 
 # 모델 구조 정의 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Conv2D(64, (3, 3), input_shape=X_train.shape[1:], padding='same'))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), padding='same'))
 model.add(Activation('relu'))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(256, (3, 3)))
 model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # 전결합층
 model.add(Flatten())    # 벡터형태로 reshape
+model.add(Dense(512))   # 출력
+model.add(Activation('relu'))
 model.add(Dense(512))   # 출력
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
@@ -59,8 +62,9 @@ model.add(Dense(classes_number))
 model.add(Activation('softmax'))
 
 # 모델 구축하기
+adam = optimizers.Adam(lr = 0.001)
 model.compile(loss='categorical_crossentropy',   # 최적화 함수 지정
-    optimizer='rmsprop',
+    optimizer='adam',
     metrics=['accuracy'])
 
 # 모델 확인
@@ -68,7 +72,7 @@ print(model.summary())
 
 # 학습 완료된 모델 저장
 hdf5_file = "./food_model.hdf5"
-model.fit(X_train, y_train, batch_size=32, epochs=100)
+model.fit(X_train, y_train, batch_size=32, epochs=100, validation_data=(X_test, y_test))
 model.save_weights(hdf5_file)
 
 
